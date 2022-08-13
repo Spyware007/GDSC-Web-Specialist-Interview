e07dcdd72424bb20cd28cd099230886f40b2bf9d
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-08-14 02:55:55 +0530

    spectacle init

---
+++ spectacle-tutorial/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Deck, Slide, Heading } from "spectacle";
+
+function App() {
+  return (
+    <>
+      <Deck>
+        <Slide>
+          <Heading>Welcome to Spectacle</Heading>
+        </Slide>
+        <Slide>
+          <Heading>Welcome to </Heading>
+        </Slide>
+        <Slide>
+          <Heading>Welcome </Heading>
+        </Slide>
+        <Slide>
+          <Heading>Welcome to Jungle </Heading>
+        </Slide>
+      </Deck>
+    </>
+  );
+}
+
+export default App;
